5dc0ac16ef6320598e70e0e9c39f18444af5653d
Rodrigo A. Silva <rgbrasil2@gmail.com>
2024-07-23 14:56:00 -0300

    adding lombok

--- src/main/java/com/rodrigo/batch_processing/BatchProcessingApplication.java
+++ src/main/java/com/rodrigo/batch_processing/BatchProcessingApplication.java
@@ -2,8 +2,6 @@ package com.rodrigo.batch_processing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class BatchProcessingApplication {
--- src/main/java/com/rodrigo/batch_processing/domain/CardType.java
+++ src/main/java/com/rodrigo/batch_processing/domain/CardType.java
@@ -1,41 +1,21 @@
 package com.rodrigo.batch_processing.domain;
 
-public class CardType {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Entity
+@Builder
+public class CardType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private Integer product;
     private Integer variant;
     private String description;
-
-    public CardType() {
-    }
-
-    public CardType(Integer product, Integer variant, String description) {
-        this.product = product;
-        this.variant = variant;
-        this.description = description;
-    }
-
-    public Integer getProduct() {
-        return product;
-    }
-
-    public void setProduct(Integer product) {
-        this.product = product;
-    }
-
-    public Integer getVariant() {
-        return variant;
-    }
-
-    public void setVariant(Integer variant) {
-        this.variant = variant;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
